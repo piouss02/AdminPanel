c8167dbfd626708e5c8358e00599cb892d67f432
Pious Shrivastava <piousshrivastava.ps@gmail.com>
2024-01-29 17:38:57 +0530

    Create new AdminPanel Repository

---
+++ src/test/java/com/OmNamahShivay/AdminPanel/AdminPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OmNamahShivay.AdminPanel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminPanelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
